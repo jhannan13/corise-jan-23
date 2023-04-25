c6961114f96728d04f4a2da056e0fe165fa04a76
Jake Hannan <jhannan08@gmail.com>
2023-04-25 07:37:25 -0700

    week 2

--- greeenery/models/staging/postgres/stg_postgres__orders.sql
+++ greeenery/models/staging/postgres/stg_postgres__orders.sql
@@ -22,6 +22,19 @@ with orders_source as (
     , estimated_delivery_at::timestampntz as order_estimated_delivery_at_utc
     , delivered_at::timestampntz as order_delivered_at_utc
   from orders_source
+  union all
+  select
+    '8329a65b-7ddf-4250-aeee-bd625f8a401a' as order_guid
+    , null as order_user_guid
+    , null as order_cost
+    , null as order_shipping_cost
+    , null as order_total_cost
+    , null as order_status
+    , null as order_promo_desc
+    , null as order_created_at_utc
+    , null as order_estimated_delivery_at_utc
+    , null as order_delivered_at_utc
+  from orders_source
 )
 
 select * from renamed_recast
